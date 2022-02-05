dbf2ba751c1711cbe5f1aa57878903fcbac0aaef
DiegoMeza <hi@diego.pro>
2022-02-05 08:36:36 -0600

    more comments and param indentation

--- src/main.ts
+++ src/main.ts
@@ -74,19 +74,20 @@ export function IteratorPath(path: string, key: string): string {
  *
  * Graph traversal for js arrays and objects.
  *
- * @param {object}              obj       - The object to traverse recursively.
- *                                          NOTE: this changes on every recursion level,
- *                                          so for an object { foo: 'bar' } -> the first recursion
- *                                          [obj] variable will hold the whole given object,
- *                                          the second recursion will explore the property 'foo' and [obj] variable will hold the value 'bar'.
- * @param   {(r: any) => void}  calllback - The function to call on each value of the given object. NOTE: is also called on the root object.
- * @param   {string}            key       - Hold the key of the current property being explored on the graph traversal operation. NOTE: is null on the first recursion.
- * @param   {object}            parent    - Hold the current parent node of the current property being explored on the graph traversal operation.
- * @param   {any}               result    - Hold the result of the last execution of the given callback and provides it for each call on the graph traversal.
- * @returns {void}                        - The correspondent XML string of the given object.
+ * @param {object}              obj        - The object to traverse recursively.
+ *                                           NOTE: this changes on every recursion level,
+ *                                           so for an object { foo: 'bar' } -> the first recursion
+ *                                           [obj] variable will hold the whole given object,
+ *                                           the second recursion will explore the property 'foo' and [obj] variable will hold the value 'bar'.
+ * @param   {(r: any) => void}  callback   - The function to call on each value of the given object. NOTE: is also called on the root object.
+ * @param   {string}            key        - Hold the key of the current property being explored on the graph traversal operation. NOTE: is null on the first recursion.
+ * @param   {object}            parent     - Hold the current parent node of the current property being explored on the graph traversal operation.
+ * @param   {any}               lastResult - Hold the result of the last execution of the given callback and provides it for each call on the graph traversal.
+ * @returns {void}                         - The correspondent XML string of the given object.
  */
 export function WalkObject(
-    obj: any, callback: (r: any) => void,
+    obj: any,
+    callback: (r: any) => void,
     key: string|null = null,
     path: string = '',
     parent: any = null,
@@ -96,7 +97,11 @@ export function WalkObject(
     // So if this is the first run it will be the given object
     // but for the next recurtions it will be the the value that holds the parent in the current key baing explored: parent[key].
     const value = (key === null) ? obj : (parent[key] || obj);
+    // lastResult will hold the return value of the callback in each recursion. NOTE: on the first recurtion lastResult is null;
     lastResult = callback({ value, path, key, parent, lastResult });
-    if (typeof(value) !== 'object') return;
+    // If current value is Walkable (if it is an array or an object): we walk the object recursivly. In js typeof([]) === typeof({}) === typeof(null) so we typecheck;
+    if (typeof(value) !== 'object' && value !== null) return;
+    // Array and Objects can be iterable through Object.keys(value) so: for each key of the value we walk recursivly.
+    // NOTE: Object.keys(['a', 'b', 'c']) === [0, 1, 2], and for common objects: Object.keys({a: 0, b: 1, c: 2}) is ['a', 'b': 'c'].
     Object.keys(obj).forEach(k => WalkObject(obj[k], callback, k, IteratorPath(path, k), obj, lastResult));
 }
